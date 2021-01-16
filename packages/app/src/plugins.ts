fa55e967b4bcaec9e13aa1803ac5a1f0399e62ff
alexkosj <alexkosj@gmail.com>
2021-01-16 15:35:13 -0800

    run yarn create-plugin

--- packages/app/src/plugins.ts
+++ packages/app/src/plugins.ts
@@ -43,3 +43,4 @@ export { plugin as PagerDuty } from '@backstage/plugin-pagerduty';
 export { plugin as Buildkite } from '@roadiehq/backstage-plugin-buildkite';
 export { plugin as Search } from '@backstage/plugin-search';
 export { plugin as Org } from '@backstage/plugin-org';
+export { plugin as TerraformCloud } from '@backstage/plugin-terraform-cloud';
